b65f649a0bcdc33d3983590c5c86f01becff667e
vinimllr <viniciusrodriguesmuller@gmail.com>
2024-11-19 22:30:40 -0300

    tst

--- ProjetoExemplo/obj/Debug/net8.0/ProjetoExemplo.AssemblyInfo.cs
+++ ProjetoExemplo/obj/Debug/net8.0/ProjetoExemplo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoExemplo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8545e6d553f0a6fa00c36bd40f16f768d5d0300d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62b524d9cec908f49aad6a58405d793a7862438c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoExemplo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoExemplo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
